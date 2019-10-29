1acf2c7bea9c97e7be3a67492519346d8c5135ee
Your Name <you@example.com>
2019-10-29 16:54:25 +0800

    test2.0

--- test/test.cpp
+++ test/test.cpp
@@ -2,10 +2,37 @@
 //
 
 #include "stdafx.h"
+#include <windows.h>
+
+//游戏功能
+//1.实现上下左右移动
+//2.吃东西长大
+//3.撞墙撞自己死亡
+//4.打印得分
+
+void Init(){
+	
+}
+
+void GameLoop(){
+	while(1){
+		
+		Sleep(100);
+	}
+}
+
+void Score(){
+
+}
 
 int main(int argc, char* argv[])
 {
-	printf("Hello World!\n");
+	Init();//初始化
+
+	GameLoop(); //游戏主循环
+
+	Score();  //打印得分
+//	printf("Hello World!\n");
 	return 0;
 }
 
